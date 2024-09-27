2673eb9a3fdefde1d0a0a4d50698b27b4fae02e6
netali1053 <natalihot1991@mail.ru>
2024-09-27 11:16:12 +0300

    Список покупок

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const inputGroceries = document.querySelector('#input');
+const itemsContainer = document.querySelector('#items');
+const classDone = document.querySelector('.done');
+
+
+inputGroceries.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const messageText = inputGroceries.value;
+
+        const newMessage = document.createElement('div');
+        newMessage.textContent = messageText;
+
+        newMessage.addEventListener('click', function() {
+            newMessage.classList.toggle('done');
+        })
+
+        if (messageText != "") {
+            itemsContainer.append(newMessage);
+        }
+
+        inputGroceries.value = '';
+    }
+});
+
+
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
